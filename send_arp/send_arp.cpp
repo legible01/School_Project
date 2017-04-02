d931dda0404d7be10bb6b21a7704c3bfacbab0c2
KKS <legible01@gmail.com>
2017-04-03 04:07:15 +0900

    fix about overlaped codes

--- send_arp/send_arp.cpp
+++ send_arp/send_arp.cpp
@@ -37,7 +37,8 @@ typedef struct packet_info{
     uint8_t target_mac[6];
     }PACKET_INFO;
 
-int get_my_info(char * dev_name ,uint8_t *struc_p,u_char *struc_mac);
+void get_my_info(char * dev_name ,uint8_t *struc_p,u_char *struc_mac);
+void check_ioctl_err(int ioctl_flag);
 void get_mac_addr(pcap_t * pack_d,PACKET_INFO *pack_buf, uint8_t *input_ip, uint8_t *recv_mac);//pd,&pack_info,pack_info.sender_ip
 int ether_check(uint8_t *pack1);
 int arp_check(uint8_t *pack2,uint8_t *req_ip,uint8_t *input_mac);//,uint8_t *struc_info_mac,
@@ -46,11 +47,12 @@ void request_arp_info(struct arp_hdr *arp_p,PACKET_INFO *arp_info,uint8_t *ip_ad
 void reply_ether_info(struct ether_header *eth_p,PACKET_INFO * eth_info);
 void reply_arp_info(struct arp_hdr *arp_p,PACKET_INFO *arp_info);
 void send_arp_packet(uint8_t *send_arp,pcap_t *pack_d,int packet_size);
+void arp_common_info(struct arp_hdr *arp_p,PACKET_INFO *arp_info);
 
 
 int main(int argc, char *argv[])
 {
-   //at first argument num is  "filename dev senderIp targetIp sendermac targetmac"
+   //at first argument num is  "filename dev senderIp targetIp"
     if (argc !=4){
         printf("\nplease check your argument form\n");
         printf("arp_send [device] [sender ip(1.1.1.1)] [target ip(1.1.1.1)]\n");
@@ -61,8 +63,6 @@ int main(int argc, char *argv[])
     pack_info.dev_name = argv[1];//input device in structure
     inet_pton(AF_INET,argv[2],pack_info.sender_ip);
     inet_pton(AF_INET,argv[3],pack_info.target_ip);
-
-
     get_my_info(argv[1],&pack_info.my_ip[0],&pack_info.my_mac[0]);
 
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -72,12 +72,14 @@ int main(int argc, char *argv[])
     get_mac_addr(pd,&pack_info,pack_info.target_ip,pack_info.target_mac);
 
 
+
     uint8_t send_buffer[sizeof(ether_header)+sizeof(arp_hdr)];
 
     struct ether_header *eth_hdr_p = (struct ether_header *)&send_buffer;
     struct arp_hdr *arp_hdr_p =(struct arp_hdr *) &send_buffer[sizeof(ether_header)];
 
     reply_ether_info(eth_hdr_p,&pack_info);
+    arp_common_info(arp_hdr_p,&pack_info);
     reply_arp_info(arp_hdr_p,&pack_info);
 
     send_arp_packet((uint8_t*)&send_buffer,pd,sizeof(send_buffer));
@@ -88,29 +90,36 @@ int main(int argc, char *argv[])
 
 
 
-int get_my_info(char * dev_name ,uint8_t *struc_p,u_char *struc_mac)
+void get_my_info(char * dev_name ,uint8_t *struc_p,u_char *struc_mac)
 {
 
     int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (fd < 0) {
         printf("socket error\n");
-        return -1;
+        return ;
     }
 
     struct ifreq ifr;
     strcpy(ifr.ifr_name, dev_name);
-    if (ioctl(fd, SIOCGIFADDR, &ifr) < 0){
-        printf("IpAddr ioctl() error\n");                       // Get IP Adress
-        return -1;
-    }
-    memcpy(struc_p,ifr.ifr_addr.sa_data+2,sizeof(uint32_t));
 
-    if (ioctl(fd, SIOCGIFHWADDR, &ifr) < 0){
-        printf("MAC ioctl() error\n");                          //get mac addr
-        return -1;
+    int result = ioctl(fd, SIOCGIFADDR, &ifr);
+    check_ioctl_err(result);
+    result = ioctl(fd, SIOCGIFHWADDR, &ifr);
+    check_ioctl_err(result);
+
+    memcpy(struc_p,ifr.ifr_addr.sa_data+2,sizeof(uint32_t));// Get IP Adress
+    memcpy(struc_mac,ifr.ifr_hwaddr.sa_data,sizeof(u_char)*6);//get mac addr
+
+
+}
+
+void check_ioctl_err(int ioctl_flag)
+{
+    if (ioctl_flag < 0){
+        printf("getHardInfo Error");
+        exit(1);
     }
-    memcpy(struc_mac,ifr.ifr_hwaddr.sa_data,sizeof(u_char)*6);
-    return 0;
+    return;
 }
 
 
@@ -118,6 +127,7 @@ void get_mac_addr(pcap_t * pack_d,PACKET_INFO *pack_buf, uint8_t *input_ip, uint
 {
     uint8_t arp_req_buf[sizeof(ether_header)+sizeof(arp_hdr)];                                            //edit this part
     request_ether_info((struct ether_header *)&arp_req_buf,pack_buf);
+    arp_common_info((struct arp_hdr *)&arp_req_buf[sizeof(ether_header)],pack_buf);
     request_arp_info((struct arp_hdr *)&arp_req_buf[sizeof(ether_header)],pack_buf,input_ip);
 
 
@@ -192,13 +202,9 @@ void request_arp_info(struct arp_hdr *arp_p,PACKET_INFO *arp_info,uint8_t *ip_ad
 {
     memcpy(arp_p->ar_sip,arp_info->my_ip,sizeof(uint8_t)*4);//my ip->sender ip **
     memcpy(arp_p->ar_tip,ip_addr,sizeof(uint8_t)*4);//sender ip->target ip
-    memcpy(arp_p->ar_sha,arp_info->my_mac,sizeof(uint8_t)*6);//my mac->sender hw
     for(int i=0;i<6;i++)
-        arp_p->ar_tha[i]=0;                         //sender mac->target hw **
-    arp_p->ar_hrd = htons(0x0001);
-    arp_p->ar_pro = htons(0x0800);
-    arp_p->ar_hln=6;
-    arp_p->ar_pln=4;
+        arp_p->ar_tha[i]=0;                         //target hw 0
+
     arp_p->ar_op = htons(ARP_REQUEST);//if i send arp request then need it **
 
 }
@@ -217,15 +223,18 @@ void reply_arp_info(struct arp_hdr *arp_p,PACKET_INFO *arp_info)
 {
     memcpy(arp_p->ar_sip,arp_info->target_ip,sizeof(uint8_t)*4);//gateway ip->sender ip
     memcpy(arp_p->ar_tip,arp_info->sender_ip,sizeof(uint8_t)*4);//sender ip->target ip
-    memcpy(arp_p->ar_sha,arp_info->my_mac,sizeof(uint8_t)*6);//my mac->sender hw
     memcpy(arp_p->ar_tha,arp_info->sender_mac,sizeof(uint8_t)*6);//sender mac->target hw
+    arp_p->ar_op = htons(ARP_REPLY);//if i send arp request then need it
+}
+
+void arp_common_info(struct arp_hdr *arp_p,PACKET_INFO *arp_info){
+    memcpy(arp_p->ar_sha,arp_info->my_mac,sizeof(uint8_t)*6);//my mac->sender hw
     arp_p->ar_hrd = htons(0x0001);
     arp_p->ar_pro = htons(0x0800);
     arp_p->ar_hln=6;
     arp_p->ar_pln=4;
-    arp_p->ar_op = htons(0x0002);//if i send arp request then need it
-}
 
+}
 
 void send_arp_packet(uint8_t *send_arp,pcap_t *pack_d,int packet_size)
 {
